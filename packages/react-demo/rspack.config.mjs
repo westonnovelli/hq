f64b6c828bf57b44307793e2fa33635fb8f6457b
Weston Novelli <weston@novelli.dev>
2023-07-28 22:31:15 -0500

    build fix

--- packages/react-demo/rspack.config.mjs
+++ packages/react-demo/rspack.config.mjs
@@ -1,23 +1,26 @@
 import path from 'path';
 
 export default {
-  target: 'web',
-  entry: {
-    main: './src/index.ts',
-  },
-  output: {
-    filename: 'main.js',
-    path: path.resolve('dist'),
-    library: {
-        type: 'module',
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        type: 'tsx',
-      },
-    ],
-  },
+    target: 'web',
+    entry: {
+        main: './src/index.ts',
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve('dist'),
+        library: {
+            type: 'module'
+        },
+    },
+    experiments: {
+        outputModule: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                type: 'tsx',
+            },
+        ],
+    },
 };
