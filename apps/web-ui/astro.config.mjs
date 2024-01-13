2e33b263dd839cf6b69041e65e32c11ce24e7f97
Weston Novelli <weston@novelli.dev>
2024-01-12 20:22:08 -0600

    architect-for-change-draft

--- apps/web-ui/astro.config.mjs
+++ apps/web-ui/astro.config.mjs
@@ -1,11 +1,23 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import AutoImport from 'astro-auto-import';
+import MDXCodeBlocks, { mdxCodeBlockAutoImport } from 'astro-mdx-code-blocks';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
-  site: 'https://weston.novelli.dev',
-  outDir: '../../dist',
+    integrations: [
+        tailwind(),
+        react(),
+        AutoImport({
+            imports: [
+                mdxCodeBlockAutoImport('./src/components/CodeBlock.astro')
+            ],
+        }),
+        MDXCodeBlocks(),
+        mdx()
+    ],
+    site: 'https://weston.novelli.dev',
+    outDir: '../../dist',
 });
 
--- apps/web-ui/src/pages/thoughts/_template.md
+++ apps/web-ui/src/pages/thoughts/_template.md
@@ -2,5 +2,5 @@
 layout: '../../components/blog/post.astro'
 title: ''
 publishedDate: '' # :r!date => Fri Jul 28 09:27:59 PM CDT 2023
-draft: true
+draft: true #
 ---
