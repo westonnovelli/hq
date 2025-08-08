58b366ffec5b8eb9f8c0fe8321775bc66e6bd2e0
Weston Novelli <weston@novelli.dev>
2025-08-08 16:15:56 -0500

    Move interest button to landing page

--- apps/web-ui/src/env.d.ts
+++ apps/web-ui/src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_POSTHOG_KEY?: string;
+  readonly PUBLIC_POSTHOG_HOST?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
