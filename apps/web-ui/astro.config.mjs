80d00695484e9e850b6174c426880f46fe48d263
Weston Novelli <weston@novelli.dev>
2025-12-20 08:11:28 -0600

    snippets, colors, astro5

--- apps/web-ui/astro.config.mjs
+++ apps/web-ui/astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 import AutoImport from 'astro-auto-import';
 import MDXCodeBlocks, { mdxCodeBlockAutoImport } from 'astro-mdx-code-blocks';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+    output: 'server',
+    adapter: node({
+        mode: 'standalone'
+    }),
     integrations: [
         tailwind(),
         react(),
